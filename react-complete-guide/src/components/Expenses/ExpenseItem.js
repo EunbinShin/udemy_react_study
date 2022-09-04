b07576ddabdd6d3cefd1809086c3d8b8ba429678
EunbinShin <38269665+EunbinShin@users.noreply.github.com>
2022-09-04 18:52:39 +0900

    Section 4 - State 사용해보기
    State 사용하여 컴포넌트 내 값 바꾸기

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,27 @@
-import './ExpenseItem.css'
+import React, { useState } from 'react'
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Cards';
+import './ExpenseItem.css'
 
 function ExpenseItem(props){
+
+    const [title, setTitle] = useState(props.title);
+    
     const expenseDate = props.date;
-    const expenseTitle =  props.title;
     const expenseAmount =  props.amount;
+
+    const clcikHandler = () => {
+        setTitle('Updated !!!')
+    }
 
     return (
         <Card className='expense-item'>
             <ExpenseDate date={expenseDate}/>
             <div className='expense-item__description'>
-                <h2>{expenseTitle}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${expenseAmount}</div>
             </div>
+            <button onClick={clcikHandler}>Change Title</button>
         </Card>
     )
 }
