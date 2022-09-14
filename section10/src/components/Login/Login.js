efe3ac3ab633d3e492b348b952e0f93be0d25554
EunbinShin <38269665+EunbinShin@users.noreply.github.com>
2022-09-14 21:36:49 +0900

    section10 - 커스텀 컨텍스트

--- section10/src/components/Login/Login.js
+++ section10/src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useReducer, useState } from 'react';
+import React, { useContext, useEffect, useReducer, useState } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
 
 const emailReducer = (state, action) => {
   if(action.type === 'USER_INPUT'){
@@ -23,6 +24,8 @@ const passwordReducer = (state, action) => {
 }
 
 const Login = (props) => {
+  const context = useContext(AuthContext)
+
   const [formIsValid, setFormIsValid] = useState(false);
 
   const [emailState, dispatchEmail] = useReducer(emailReducer, {
@@ -77,7 +80,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    context.onLogin(emailState.value, passwordState.value);
   };
 
   return (
