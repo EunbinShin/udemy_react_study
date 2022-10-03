4098f570821da80571ce56dafe06131f6c4a44ee
EunbinShin <38269665+EunbinShin@users.noreply.github.com>
2022-10-03 15:01:39 +0900

    section 11 - context, reducer, ref, effect 활용

--- food-order-app/src/components/Meals/MealItem/MealItemForm.js
+++ food-order-app/src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useRef, useState, useContext } from 'react';
+import CartContext from '../../../store/cart-context';
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css'
 
 const MealItemForm = (props) => {
+    const amountInputRef = useRef()
+    const [amountIsValid, setAmountIsValid] = useState(true);
+
+    const submitHandler = (event) => {
+        event.preventDefault()
+
+        const enteredAmout = amountInputRef.current.value;
+        const enteredAmoutNumber = +enteredAmout;
+
+        if(enteredAmout.trim().length === 0 || enteredAmoutNumber < 1 || enteredAmoutNumber > 5){
+            setAmountIsValid(false)
+            return ;
+        }
+
+        props.onAddToCart(enteredAmoutNumber)
+    }
     return (
-        <form className={classes.form}>
+        <form className={classes.form} onSubmit={submitHandler}>
             <Input 
                 label="Amount" 
                 input={{
@@ -14,8 +31,10 @@ const MealItemForm = (props) => {
                     'max': '5',
                     'step': '1',
                     'defaultValue': '1'
-                }}/>
+                }}
+                ref={amountInputRef}/>
             <button>+ Add</button>
+            {!amountIsValid && <p>Please enter a valid amount (1-5)</p>}
         </form>
     );
 };
